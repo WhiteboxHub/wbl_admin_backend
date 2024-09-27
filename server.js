b6b89d256378768963d7b0ca490781f6ecfe1027
shivapatel7 <shivapatel098k@gmail.com>
2024-09-27 10:44:00 -0700

    batchbackend

--- server.js
+++ server.js
@@ -46,6 +46,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 const leadsRoutes = require('./routes/leadsRoutes');
+const batchRoutes = require('./routes/batchRoutes'); // Import the batchRoutes
 const mysql = require('mysql2');
 
 const app = express();
@@ -76,6 +77,7 @@ app.use((req, res, next) => {
 
 app.use('/api/auth', authRoutes);
 app.use('/api', leadsRoutes);
+app.use('/api', batchRoutes); // Add the batchRoutes
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
