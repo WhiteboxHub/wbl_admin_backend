0eca884bf1acb346355aacf008aa5bd7c5882d10
shivapatel7 <shivapatel098k@gmail.com>
2024-10-29 13:12:18 -0700

    added placement in backend gor pages

--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const AdminValidationMiddleware = require('../middleware/AdminValidationMiddleware');
+const AdminValidationMiddleware = require('../Middleware/AdminValidationMiddleware');
 const clientController = require('../controllers/clientController');
 // Get all POs for a client
-router.get('/client', AdminValidationMiddleware, clientController.getPOsByClient);
+router.get('/client', AdminValidationMiddleware, clientController.getClients);
 
 // Add new PO for a client
-router.post('/client/add', AdminValidationMiddleware, clientController.addPOByClient);
+router.post('/client/add', AdminValidationMiddleware, clientController.addClient);
 
 // Update PO for a client
-router.put('/client/update/:id', AdminValidationMiddleware, clientController.updatePOByClient);
+router.put('/client/update/:id', AdminValidationMiddleware, clientController.updateClient);
 
 // View PO by ID for a client
-router.get('/client/:id', AdminValidationMiddleware, clientController.viewPOByClientId);
+router.get('/client/:id', AdminValidationMiddleware, clientController.viewClientById);
 
 // Delete PO for a client
-router.delete('/client/delete/:id', AdminValidationMiddleware, clientController.deletePOByClient);
+router.delete('/client/delete/:id', AdminValidationMiddleware, clientController.deleteClient);
 
 
 module.exports = router;
